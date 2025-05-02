788a4a3a93c12eb0cad63bdefd1b380af383561c
surfish <shrshr2@163.com>
2025-05-02 22:09:34 +0800

    prepare for deploy static pages

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,6 @@ import "./style.css";
 import { Signals } from "./controller/gameSignals";
 
 const engine = new Core2D5(2);
-const game = new Game(100, engine);
+new Game(100, engine);
 
 Signals.render.notify(engine);
-
-;(async function () {
-  // engine.gridX[0][0].depth = 1;
-  // engine.gridY[0][0].depth = 1;
-  // engine.gridZ[0][0].depth = 1;
-
-  // engine.gridX[0][1].depth = 1;
-  // engine.gridY[0][1].depth = 1;
-  // engine.gridZ[0][0].depth = 2;
-  // engine.signals.render.notify(engine);
-  // await new Promise(resolve => setTimeout(resolve, 1000));
-  // game.dent(0,0,0);
-  // await new Promise(resolve => setTimeout(resolve, 1000));
-  // game.dent(0,0,1);
-  // await new Promise(resolve => setTimeout(resolve, 1000));
-  // game.jut(0,0,1);
-  // game.concave(0,0,0);
-})();
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { UserConfig } from 'vite'
+
+export default {
+  base: 'puzzle2d5',
+  build: {
+    outDir: 'docs',
+  }
+} satisfies UserConfig;
